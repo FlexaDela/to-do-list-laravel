de6e2b04b855a80826d21cfa1407f0537251c4a8
FlexaDela <mateusflexa2000@gmail.com>
2026-04-17 11:43:08 -0300

    arquivos necessarios para a criação da substask

---
+++ app/Http/Controllers/SubTaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSubTaskRequest;
+use App\Http\Requests\UpdateSubTaskRequest;
+use App\Models\SubTask;
+
+class SubTaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSubTaskRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SubTask $subTask)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SubTask $subTask)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSubTaskRequest $request, SubTask $subTask)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SubTask $subTask)
+    {
+        //
+    }
+}
