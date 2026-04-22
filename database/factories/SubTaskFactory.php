2608d27bed168015e47b7352efec44e703b52e96
FlexaDela <mateusflexa2000@gmail.com>
2026-04-22 09:48:44 -0300

    criando seeders e factorys para tasks e subtasks

---
+++ database/factories/SubTaskFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SubTask;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<SubTask>
+ */
+class SubTaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
