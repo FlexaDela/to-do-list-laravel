f9190f078d40014ca2338d3a7a2b42fa403d282a
FlexaDela <mateusflexa2000@gmail.com>
2026-04-26 20:18:04 -0300

    erro do bug de error no edit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,7 +5,7 @@
             @if ($errors->any())
                 <x-alert type="danger">
                     <ul>
-                        @foreach ($erros->all() as $error)
+                        @foreach ($errors->all() as $error)
                             <li> {{ $error }} </li>
                         @endforeach
                     </ul>
